633a33557aab3a8f02b940e467a042a24ebe7f6c
Ernesto Torres <ejtm.193@gmail.com>
2023-06-05 14:43:18 +0000

    update appJs file logic

--- Vschool/PreCourseProject/resources/app.js
+++ Vschool/PreCourseProject/resources/app.js
@@ -1,6 +1,26 @@
+//Login Place holder
+var Ernesto = {
+    userName: 'ernesto.torres1',
+    passWord: 'password'
+}
+var guest = {
+    userName: 'guest',
+    passWord: 'password'
+}
+
+let user = [Ernesto, guest,]
+
+let userInput1 = document.getElementById("userName")
+let userInput2 = document.getElementById("password")
 let logInBtn = document.getElementById("logInBtn")
-console.log(logInBtn)
+
 function onClick(){
-    window.location.href = "404.html"
+    for(var i = 0; i < user.length; i++){
+        if(userInput1 === user[i].userName || userInput2 === user[i].passWord){
+            window.location.href = "404.html"
+        } else {
+            alert("Incorrect Log In")
+        }
+    }
 }
 logInBtn.addEventListener("click", onClick)
