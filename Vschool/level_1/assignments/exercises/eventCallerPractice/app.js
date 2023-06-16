7bd51f6048269160a236c1e5b92d9e88d4db9ae7
Ernesto Torres <ejtm.193@gmail.com>
2023-06-16 14:40:20 +0000

    fml

--- Vschool/level_1/assignments/exercises/eventCallerPractice/app.js
+++ Vschool/level_1/assignments/exercises/eventCallerPractice/app.js
@@ -1,11 +1,5 @@
 var square = document.getElementById("square");
-
-var redBtn = document.getElementById("red");
-var blueBtn = document.getElementById("blue");
-var greenBtn = document.getElementById("green");
-var yellowBtn = document.getElementById("yellow");
-var orangeBtn = document.getElementById("orange");
-var btnValue = [
+var cValue = [
         red = document.getElementById("red").value,
         blue = document.getElementById("blue").value,
         yellow = document.getElementById("yellow").value,
@@ -13,10 +7,44 @@ var btnValue = [
         orange = document.getElementById("orange").value,
     ]
 
+//button logic - done
 var buttons = document.getElementsByTagName("button");
 function onClick(event){
-    console.log(event.target.id);
+    for(var i = 0; i < cValue.length; i++)
+    if(event.target.id == cValue[i]){
+        // console.log(btnValue[i] + "1")
+        // console.log(event.target.id); TESTS WORK
+        square.style.backgroundColor = cValue[i];
+    }
+
 }
-for(let button of buttons){
+for(var button of buttons){
     button.addEventListener("click", onClick)
+}
+
+//text logic
+
+// var text = document.getElementById("textOption").value
+// function typed(event){
+//     for(var i = 0; i < btnValue.length; i++)
+//     if(event.target.text == btnValue[i]){
+//         // console.log(btnValue[i] + "1")
+//         // console.log(event.target.id); TESTS WORK
+//         square.style.backgroundColor = btnValue[i];
+//     }
+// }
+// for(var typeIn of text){
+//     text.addEventListener("keyStroke", typed)
+// }
+var textIn = document.getElementById("textOption").value
+function textE(event){
+    for(var i = 0; i < cValue.length; i++){
+        if(event.value == cValue[i]){
+            console.log(cValue)
+            return cValue
+        }
+    }
+}
+HTMLInputElement.addEventListener('input', textE()){
+    
 }
