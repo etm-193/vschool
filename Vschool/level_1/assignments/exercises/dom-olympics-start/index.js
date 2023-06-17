ac4bc14cb9aac69159e270b1b2aca287fe0710d0
Ernesto Torres <ejtm.193@gmail.com>
2023-06-17 18:32:22 +0000

    Bronze Completed

---
+++ Vschool/level_1/assignments/exercises/dom-olympics-start/index.js
@@ -0,0 +1,31 @@
+//step 1 - header
+var bronze = "Brought to you by JavaScript!"
+var header = document.getElementById('header');
+header.innerHTML = bronze;
+
+header.style.fontSize = "30px";
+
+//step 2 - credit
+
+var devName = "Ernesto Torres "
+var credStat = "wrote the JavaScript."
+
+var span = document.createElement('span');
+var node = document.createTextNode(devName);
+
+span.appendChild(node)
+// node.style.color = "blue";
+var spanTwo = document.createElement('span');
+var nodeTwo = document.createTextNode(credStat)
+spanTwo.appendChild(nodeTwo) 
+
+var div = document.createElement("div");
+header.appendChild(div)
+div.appendChild(span)
+div.appendChild(spanTwo)
+
+span.style.color = "blue"
+span.style.fontSize = "20px"
+spanTwo.style.fontSize = "20px"
+div.style.padding = "5px"
+//------------------------------------------------------- BRONZE COMPLETED
