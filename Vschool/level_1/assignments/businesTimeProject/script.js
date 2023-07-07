bbc4798595b87fab6cab001842b08c968e646f47
Ernesto Torres <ejtm.193@gmail.com>
2023-07-07 12:47:03 -0500

    buildingBlocks
    Added my pro-port project to add more content to the site and help with my project

---
+++ Vschool/level_1/assignments/businesTimeProject/script.js
@@ -0,0 +1,7 @@
+// window.onscroll = function(){scrollFunc()}
+
+// function scrollFunc(){
+//     if(document.body.scrollTop > 50){
+//         document.getElementById("logo").style.width = 
+//     }
+// }
