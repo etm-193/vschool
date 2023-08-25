e1c074df4b2c108a40ef362250b1a0acf6f757f8
Ernesto Torres <ejtm.193@gmail.com>
2023-08-24 22:26:49 -0500

    challenge 2 completed
    fml took forever but completed it i needed a separate variable arr and lenght to be separate.

--- Vschool/level_1/assignments/exercises/e-midLevelAssesmet/main.js
+++ Vschool/level_1/assignments/exercises/e-midLevelAssesmet/main.js
@@ -25,3 +25,33 @@ console.log(book1.getDetails()); // Expected output: Title: The Great Gatsby, Au
 
 const book2 = new Book("To Kill a Mockingbird", "Harper Lee", 1960);
 console.log(book2.getDetails()); // Expected output: Title: To Kill a Mockingbird, Author: Harper Lee, Year: 1960
+
+/*
+https://codepen.io/ernest_jtorres/pen/LYMpVXw
+
+note when console log detects each instatntiation of a book object it tries to run two console logs one outside of the internal constructor function and one outside that will return an undefined to fix: console.log(book.getDetails()); --> book.getDetails();
+
+*/
+
+/* challenge 2: Arrays and Loops
+
+Write a function called **`removeDuplicates`** that takes an array of numbers as input and returns a new array with duplicate values removed.  Use the below code to test your solution.
+*/ 
+
+
+
+function removeDuplicates(numArr){
+    let arr = [];
+    let length = numArr.length;
+    for(let i = 0; i < length; i++){
+        if(arr.indexOf(numArr[i]) === -1){
+            arr.push(numArr[i]);
+        }
+    
+    }
+    console.log(arr)
+}
+
+let newArr = [1,1,2,2,3,4,5,5,6,7,8,9,9, 10, 10]
+
+removeDuplicates(newArr);
