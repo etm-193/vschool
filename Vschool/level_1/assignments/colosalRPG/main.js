a14fb458d8296c459981400868dc86690ce064d1
Ernesto Torres <ejtm.193@gmail.com>
2023-09-20 22:47:38 -0500

    workshopHelp2

--- Vschool/level_1/assignments/colosalRPG/main.js
+++ Vschool/level_1/assignments/colosalRPG/main.js
@@ -3,21 +3,21 @@ const readline = require('readline-sync');
 
 let isGameRunning = true;
 
-function Character(name){
+function Character(name, health, inventory){
     this.name = name;
     // this.lvl = lvl;
     this.health = health;
     // this.atk = atk;
     // this.def = def;
-    let inventory = [];
+    this.inventory = [];
 }
 
 const playerName = readline.question("input name: ") // working - need to edit
-const player = new Character(playerName, 100, playerInventory);
+const player = new Character(playerName, 100,);
 const enemy1 = new Character("Annoying Imp", 15, ["Dagger", "gold coins"]);
 const enemy2 = new Character("Troll Order", 30, ["Great Sword", "Troll Snot", "troll nails"]);
 const enemy3 = new Character("Frustrated Developer", 90, ["keyboard hammer","mouse whip", "monitor-rent talisman", "Coffee"]);
-let playerInventory = [];
+
 let enemies = [enemy1,enemy2,enemy3];
 
 const randomNumber = (min, max)=>{
@@ -28,7 +28,7 @@ const welcome = `Welcome ${player.name}...Says a nerdy yet handsom looking devel
 
 console.log(welcome);
 console.log("you fall through a trap door. When you land surprisingly unharmed you look around and find yourself in a fire lit corridor. Hey look there is a stick on the floor, maybe you can whack someone with it.");
-playerInventory.push("stick")
+player.inventory.push("stick")
 while(isGameRunning){
     let action = readline.question("What do you do? Press[w] to walk, [i] for inventory, or [q] to quit.",{limit:["w","i","q"]});
 
@@ -53,7 +53,7 @@ while(isGameRunning){
         }
 
     } else if(action === "i"){
-     
+     console.log(player.inventory)
         
     } else if(action === "q"){
         console.log(`you chose to quit ${player.name}... insert horrible death here`);
