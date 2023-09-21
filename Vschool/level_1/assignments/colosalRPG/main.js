9b28a414258aff32cd758b8853204ac0410fb7f0
Ernesto Torres <ejtm.193@gmail.com>
2023-09-20 21:52:51 -0500

    workshopHelp1

--- Vschool/level_1/assignments/colosalRPG/main.js
+++ Vschool/level_1/assignments/colosalRPG/main.js
@@ -1,68 +1,69 @@
 //colosal rpg game!
 const readline = require('readline-sync');
 
+let isGameRunning = true;
 
-//objects and global variables
-let gameOver = false; 
-
-function Player(name){
+function Character(name){
     this.name = name;
     // this.lvl = lvl;
-    // this.health = health;
+    this.health = health;
     // this.atk = atk;
     // this.def = def;
+    let inventory = [];
 }
 
-// function Enemy(enemy){
-//     this.lvl = lvl;
-//     this.atk = atk;
-//     this.def = def;
-//     this.health = health;
-// }
-
-const instructionOne = `Use w to move forward: `;
-
-
-//logic
-function moving(){
-    let encounterChance = Math.random() * 5;
-    if (encounterChance == 1 || encounterChance == 3 || encounter == 5 ){
-        console.log("battle!")
-        return encounterChance;
-    } else {
-        console.log("Nothing yet")
-        return encounterChance;
-    }
-
-
-}
-function walk(){
-    if(readline.keyIn("", {limit:'w'})){
-        moving()
-        let encounter = moving();
-        if(encounter == 0 ||encounter == 2 ||encounter == 4){
-            console.log("you live")
-            return encounter
-
-    }
-}
-
-//game time
 const playerName = readline.question("input name: ") // working - need to edit
-const player = new Player(playerName)
+const player = new Character(playerName, 100, playerInventory);
+const enemy1 = new Character("Annoying Imp", 15, ["Dagger", "gold coins"]);
+const enemy2 = new Character("Troll Order", 30, ["Great Sword", "Troll Snot", "troll nails"]);
+const enemy3 = new Character("Frustrated Developer", 90, ["keyboard hammer","mouse whip", "monitor-rent talisman", "Coffee"]);
+let playerInventory = [];
+let enemies = [enemy1,enemy2,enemy3];
 
-const welcome = `Welcome ${player.name}`;
+const randomNumber = (min, max)=>{
+    return Math.floor(Math.random()*max - min +1);
+}
 
+const welcome = `Welcome ${player.name}...Says a nerdy yet handsom looking developer (T_T) as he inches to away from you. suddenly he yells, "FRESH MEAT FOR THE GRINDER BOYS!" and hits a switch`;
 
-//do{
 console.log(welcome);
-//console.log(instructionOne)
-//if(readline.keyIn(instructionOne, {limit: 'w'})){
-//    console.log(`nice ${player.name} you are walking now!`)
-//} 
+console.log("you fall through a trap door. When you land surprisingly unharmed you look around and find yourself in a fire lit corridor. Hey look there is a stick on the floor, maybe you can whack someone with it.");
+playerInventory.push("stick")
+while(isGameRunning){
+    let action = readline.question("What do you do? Press[w] to walk, [i] for inventory, or [q] to quit.",{limit:["w","i","q"]});
 
-// do{
-//     walk()
-//     let game = walk()
-//     if(game == 0 || game== 2 || game== 4){gameOver=true;}
-// }    while(gameOver == false)
+    if(action === "w"){
+        console.log("you move forward");
+        let enemyChance = randomNumber(1, 4);
+        console.log(enemyChance);
+        
+        if(enemyChance === 3){
+            let randomEnemyIndex = randomNumber(0, enemies.length -1);
+            let currEnemy = enemies[randomEnemyIndex];
+            console.log(`${currEnemy.name} has appeared`);
+
+            const options = ["fight", "run"]
+            let fightOrRun = readline.keyInSelect(options,"fight or run? ")
+            console.log(options[fightOrRun]);
+            if(options[fightOrRun] === "fight"){
+                fight(currEnemy);
+            } else if(options[fightOrRun] === "run"){
+                run()
+            }
+        }
+
+    } else if(action === "i"){
+     
+        
+    } else if(action === "q"){
+        console.log(`you chose to quit ${player.name}... insert horrible death here`);
+        isGameRunning = false;
+    }
+
+}
+
+function fight(currEnemy){
+    console.log(`Get ready you are facing ${currEnemy.name}`)
+}
+
+function run(){}
